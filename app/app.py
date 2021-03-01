d5e7f5464a782914497ee39934da0574d634abe5
Cristian Radu <raduioancristian@gmail.com>
2021-03-01 17:49:11 +0100

    DEV-0003: Add GET, POST, PATCH, DELETE on endpoint /v1/users

--- app/app.py
+++ app/app.py
@@ -38,3 +38,60 @@ def hateoas(id):
 def welcome():
     # Flask way of sending ./templates/index.html.
     return render_template("index.html")
+
+
+@app.route('/v1/users', methods=['POST'])
+def post_user_details():
+    try:
+        data = request.get_json()
+        sql = text('INSERT INTO users (name, surname, identity_number) VALUES (:name, :surname, :id_num)')
+        result = db.engine.execute(
+            sql,
+            name=data['name'],
+            surname=data['surname'],
+            id_num=data['identity_number']
+        )
+        return json.dumps({"id": result.lastrowid, "links": hateoas(result.lastrowid)})
+    except Exception as e:
+        return json.dumps('Failed. ' + str(e), HTTPStatus.NOT_FOUND)
+
+
+@app.route('/v1/users/<user_id>', methods=["GET"])
+def get_user_detais(user_id):
+    try:
+        sql = text('SELECT * FROM users WHERE id=:id_num')
+        result = db.engine.execute(sql, id_num=user_id).fetchone()
+        return json.dumps({"name": result.name, "surname": result.surname, "identity_number": result.identity_number,
+                           "links": hateoas(user_id)}), HTTPStatus.OK
+    except Exception as e:
+        return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND
+
+
+@app.route('/v1/users/<user_id>', methods=['PATCH'])
+def patch_user_details(user_id):
+    data = request.get_json()
+    for key in data:
+        update_string = key + "=:" + key + ','
+        update_string = update_string[:-1]
+    try:
+        data['id'] = user_id
+        sql = text('UPDATE users SET ' + update_string + ' WHERE id = :id')
+        result = db.engine.execute(sql, data)
+        return json.dumps(
+            {
+                "id": user_id,
+                "links": hateoas(user_id)
+            }
+        )
+    except Exception as e:
+        return json.dumps("Failed. " + str(e), HTTPStatus.NOT_FOUND)
+
+
+@app.route('/v1/users/<user_id>', methods=['DELETE'])
+def delete_user_details(user_id):
+    try:
+        sql = text('DELETE FROM users WHERE id=:id_num')
+        result = db.engine.execute(sql, id_num=user_id)
+        return json.dumps('Deleted'), HTTPStatus.OK
+    except Exception as e:
+        return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND
