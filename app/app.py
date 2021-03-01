bc7e17607c7120c8c91087537c3717f4b8205e43
Cristian Radu <raduioancristian@gmail.com>
2021-03-01 17:40:52 +0100

    DEV-0003: Add database configuration files and the initial home page

--- app/app.py
+++ app/app.py
@@ -1,13 +1,40 @@
+import json
+from flask import request
+from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
+from http import HTTPStatus
+from initialise import Initialise
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+init = Initialise()
+app = init.db(app)
+
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def home():
-    return "This is the home page"
+def hateoas(id):
+    return [
+        {
+            "rel": "self",
+            "resource": "http://127.0.0.1:8000/v1/users/" + str(id),
+            "method": "GET"
+        },
+        {
+            "rel": "update",
+            "resource": "http://127.0.0.1:8000/v1/users" + str(id),
+            "method": "PATCH"
+        },
+        {
+            "rel": "update",
+            "resource": "http://127.0.0.1:8000/v1/users" + str(id),
+            "method": "DELETE"
+        }
+    ]
 
 
-@app.route("/hello")
-def hello():
-    return "Hello"
+@app.route('/')
+def welcome():
+    # Flask way of sending ./templates/index.html.
+    return render_template("index.html")
--- app/database/migration.py
+++ app/database/migration.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from ..initialise import Initialise
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root@flask-db/users"
+init = Initialise()
+app = init.db(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
