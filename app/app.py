12ee9346a51cbd27ed1735195dce7691597df472
Cristian Radu <raduioancristian@gmail.com>
2021-03-02 14:37:42 +0100

    DEV-0004: Decouple CRUD V1 from HTTP, DB, response type and write unit tests

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ from flask import render_template
 app = Flask(__name__)
 init = Initialise()
 app = init.db(app)
-
 db = SQLAlchemy(app)
 
 
@@ -43,58 +42,83 @@ def welcome():
 @app.route('/v1/users', methods=['POST'])
 def post_user_details():
     try:
-        data = request.get_json()
-        sql = text('INSERT INTO users (name, surname, identity_number) VALUES (:name, :surname, :id_num)')
-        result = db.engine.execute(
-            sql,
-            name=data['name'],
-            surname=data['surname'],
-            id_num=data['identity_number']
-        )
-        return json.dumps({"id": result.lastrowid, "links": hateoas(result.lastrowid)})
+        user_data = request.get_json()
+        insert_user(user_data)
+        return response('Added')
     except Exception as e:
         return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND
+
+
+# This decouples the HTTP request from the functionality
+def insert_user(user_data):
+    sql = text('INSERT INTO users (name, surname, identity_number) VALUES (:name, :surname, :identity_number)')
+    return execute(sql, user_data)
 
 
 @app.route('/v1/users/<user_id>', methods=["GET"])
 def get_user_details(user_id):
     try:
-        sql = text('SELECT * FROM users WHERE id=:id_num')
-        result = db.engine.execute(sql, id_num=user_id).fetchone()
-        return json.dumps({"name": result.name, "surname": result.surname, "identity_number": result.identity_number,
-                           "links": hateoas(user_id)}), HTTPStatus.OK
+        result = get_user(user_id)
+        return response({"name": result.name, "surname": result.surname, "identity_number": result.identity_number,
+                         "links": hateoas(user_id)})
     except Exception as e:
         return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND
 
 
+def get_user(user_id):
+    user_data = {}
+    user_data['id_num'] = user_id
+    sql = text('SELECT * FROM users WHERE id=:id_num')
+    return execute(sql, user_data).fetchone()
+
+
 @app.route('/v1/users/<user_id>', methods=['PATCH'])
 def patch_user_details(user_id):
-    data = request.get_json()
-    for key in data:
-        update_string = key + "=:" + key + ','
-        update_string = update_string[:-1]
+    user_data = request.get_json()
     try:
-        data['id'] = user_id
-        sql = text('UPDATE users SET ' + update_string + ' WHERE id = :id')
-        result = db.engine.execute(sql, data)
-        return json.dumps(
-            {
-                "id": user_id,
-                "links": hateoas(user_id)
-            }
-        )
+        user_data['id'] = user_id
+        update_user(user_data)
+        response({
+            "id": user_id,
+            "links": hateoas(user_id)
+        })
     except Exception as e:
         return json.dumps("Failed. " + str(e)), HTTPStatus.NOT_FOUND
+
+
+# This decouples the HTTP request from the functionality
+def update_user(user_data):
+    update_string = ""
+    for key in user_data:
+        update_string += key + "=:" + key + ','
+    update_string = update_string[:-1]
+    sql = text('UPDATE users SET ' + update_string + ' WHERE id = :id')
+    return execute(sql, user_data)
 
 
 @app.route('/v1/users/<user_id>', methods=['DELETE'])
 def delete_user_details(user_id):
     try:
-        sql = text('DELETE FROM users WHERE id=:id_num')
-        result = db.engine.execute(sql, id_num=user_id)
-        return json.dumps('Deleted'), HTTPStatus.OK
+        result = delete_user(user_id)
+        response('Deleted')
     except Exception as e:
         return json.dumps('Failed. ' + str(e)), HTTPStatus.NOT_FOUND
+
+
+def delete_user(user_id):
+    user_data = {"id_num": user_id}
+    sql = text('DELETE FROM users WHERE id=:id_num')
+    return execute(sql, user_data)
+
+
+# Decouples the database from the app logic (can switch from MySQL to Mongo)
+def execute(sql, data):
+    return db.engine.execute(sql, data)
+
+
+# Decouples return type from app logic (e.g. can switch from JSON to XML)
+def response(message):
+    return json.dumps(message), HTTPStatus.OK
 
 
 # V2 -------------------------------------------------------------------------------------------------------------------
